e79b4b2e623eb532e5dc8a023dd388523c47c89c
neikvon <neikvon@hotmail.com>
2018-10-10 21:03:21 +0800

    feat: return new version; 'package.json' not required.

--- index.js
+++ index.js
@@ -6,40 +6,45 @@ const changelog = require('./lib/lifecycles/changelog')
 const commit = require('./lib/lifecycles/commit')
 const tag = require('./lib/lifecycles/tag')
 
-module.exports = function standardVersion (argv) {
-  var pkg
-  bump.pkgFiles.forEach((filename) => {
+module.exports = async function standardVersion (argv) {
+  let pkg
+
+  bump.pkgFiles.forEach(filename => {
     if (pkg) return
-    var pkgPath = path.resolve(process.cwd(), filename)
+    const pkgPath = path.resolve(process.cwd(), filename)
     try {
       pkg = require(pkgPath)
     } catch (err) {}
   })
-  if (!pkg) {
-    return Promise.reject(new Error('no package file found'))
-  }
-  var newVersion = pkg.version
-  var defaults = require('./defaults')
-  var args = Object.assign({}, defaults, argv)
 
-  return Promise.resolve()
-    .then(() => {
-      return bump(args, pkg)
-    })
-    .then((_newVersion) => {
-      // if bump runs, it calculaes the new version that we
-      // should release at.
-      if (_newVersion) newVersion = _newVersion
-      return changelog(args, newVersion)
-    })
-    .then(() => {
-      return commit(args, newVersion)
-    })
-    .then(() => {
-      return tag(newVersion, pkg.private, args)
-    })
-    .catch((err) => {
-      printError(args, err.message)
-      throw err
-    })
+  pkg = pkg || {
+    version: '0.0.1',
+    private: true
+  }
+
+  if (argv.current) {
+    pkg.version = argv.current
+  }
+
+  let newVersion = pkg.version
+  const defaults = require('./defaults')
+  const args = Object.assign({}, defaults, argv)
+
+  try {
+    const _newVersion = await bump(args, pkg)
+    if (_newVersion) {
+      newVersion = _newVersion
+    }
+
+    await changelog(args, newVersion)
+
+    await commit(args, newVersion)
+
+    await tag(newVersion, pkg.private, args)
+
+    return newVersion
+  } catch (err) {
+    printError(args, err.message)
+    throw err
+  }
 }
